bf693bcfa645e700d985e0f411472cce7216e0d9
Douglas Moura <douglas.ademoura@gmail.com>
2023-01-17 14:22:04 -0300

    feat: atualiza todas as dependências

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const repository = require('../repositories/order-repository');
-const guid = require('guid');
+const nanoid = require('nanoid');
 const authService = require('../services/auth-service');
 
-exports.get = async(req, res, next) => {
+exports.get = async (req, res, next) => {
     try {
         var data = await repository.get();
         res.status(200).send(data);
@@ -15,14 +15,14 @@ exports.get = async(req, res, next) => {
     }
 }
 
-exports.post = async(req, res, next) => {
+exports.post = async (req, res, next) => {
     try {
         const token = req.body.token || req.query.token || req.headers['x-access-token'];
         const data = await authService.decodeToken(token);
 
         await repository.create({
             customer: data.id,
-            number: guid.raw().substring(0, 6),
+            number: nanoid(6),
             items: req.body.items
         });
         res.status(201).send({
