622560ba5b49a818b90fd44a861603ca3affb2bd
Douglas Moura <douglas.ademoura@gmail.com>
2023-01-19 10:51:08 -0300

    feat: remove aviso do linter para variável não utilizada

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use(
 )
 
 // Habilita o CORS
-app.use((req, res, next) => {
+app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header(
     'Access-Control-Allow-Headers',
